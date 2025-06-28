44c68cc5ce4187297eff43edcfac0b4b9453f701
kavos113 <kohei.hadano@gmail.com>
2025-06-28 13:41:18 +0900

    eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,32 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
 import { defineConfig } from 'eslint/config';
 
-export default defineConfig({
-  ignores: ['node_modules', 'dist', 'build'],
-  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', '@vue/typescript/recommended'],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module'
-  },
-  rules: {
-    'vue/multi-word-component-names': 'off',
-    'vue/block-lang': [
-      'error',
-      {
-        script: {
-          lang: 'ts'
+export default defineConfig([
+  { ignores: ['**/node_modules/**', '**/dist/**', '**/wailsjs/**'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    rules: {
+      'vue/no-v-html': 'off',
+      'vue/block-lang': [
+        'error',
+        {
+          script: { lang: 'ts' }
         }
-      }
-    ],
-    'vue/no-v-html': 'off',
-    'vue/block-order': [
-      'error',
-      {
-        order: ['script', 'template', 'style']
-      }
-    ]
+      ],
+      'vue/block-order': [
+        'error',
+        {
+          order: ['script', 'template', 'style']
+        }
+      ]
+    }
   }
-});
+]);
